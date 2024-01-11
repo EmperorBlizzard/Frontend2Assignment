dea404677349beb8426c6b598d567e9d4bc2ec5f
Thomas Hallström <minimoto@hotmail.com>
2024-01-11 19:25:16 +0100

    removed Emil.jsx from App.jsx

--- Frontend2Assignment/src/App.jsx
+++ Frontend2Assignment/src/App.jsx
@@ -1,13 +1,13 @@
 import Thomas from "./components/thomas";
 import Annelie from "./components/individuals/annelie";
-import Emil from "./components/sections/Emil"
+
 
 function App() {
   return (
     <div>
       <Annelie />
       <Thomas />
-      <Emil />
+      
     </div>
   );
 }
