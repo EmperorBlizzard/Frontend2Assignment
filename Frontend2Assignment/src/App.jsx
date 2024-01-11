32038fd0138614b52e31ac62d134f776555952af
MrMinken <minimoto@hotmail.com>
2024-01-11 14:04:52 +0100

    update screen

---
+++ Frontend2Assignment/src/App.jsx
@@ -0,0 +1,17 @@
+import Thomas from "./components/thomas";
+import Annelie from "./components/individuals/annelie";
+import Tina from "./Views/tina";
+import Emil from "./components/sections/Emil"
+
+function App() {
+  return (
+    <div>
+      <Annelie />
+      <Thomas />
+      <Tina />
+      <Emil />
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend2Assignment/src/components/sections/Emil.jsx
@@ -0,0 +1,7 @@
+function Emil() {
+    return ( 
+        <h1>Emil</h1>
+     );
+}
+
+export default Emil;
