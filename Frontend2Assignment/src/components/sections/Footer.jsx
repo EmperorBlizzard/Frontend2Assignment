2fc5153d3b3b27b18d388f6a06be4766a666f7c6
Tina-Lutti <tina.lutti@yh.nackademin.se>
2024-01-15 18:46:09 +0100

    uppdaterat layout footer

--- Frontend2Assignment/src/components/sections/Footer.jsx
+++ Frontend2Assignment/src/components/sections/Footer.jsx
@@ -1,63 +1,81 @@
 import React from 'react'
 import styled from "styled-components";
 
-const StyledFooter = styled.div({
-  borderTop: '1px solid var(--Border-Default, #798490)',
-  background: 'var(--Generic-White, #FFF)',
-  display: 'flex',
-  width: '100%', 
-  justifyContent: 'space-between', 
-  alignItems: 'center',
-  gap: 40,
-})
 
-const FooterHeading = styled.h5({ 
-  color: '#0B539B',
-  fontFamily: 'Inter, sans-serif', 
-  fontSize: '16px', 
-  fontStyle: 'normal',
-  fontWeight: 700,
-  lineHeight: '21px'
-})
+const FooterContent = styled.div`
+  border-top: 1px solid #798490;
+  background: #FFF;
+  display: flex;
+  height: 40px;
+  width: 100%; 
+  margin: 0 auto;
+  padding: 0 50px 300px 50px;
+  justify-content: space-between; 
+  align-items: stretch; 
+   `;
 
-const StyledParagraph = styled.p({
-color: '#2B3136',
-fontFamily: 'Inter, sans-serif',
-fontSize: '14px',
-fontStyle: 'normal',
-fontWeight: 400,
-lineHeight: '21px'
-})
+const FooterColumn = styled.div`
+  background-color: #FFF;
+  display: flex;
+  width: 240px;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 24px 50px 300px 50px;
+  flex-shrink: 0;
+  margin-top: 0;
+  `;
+
+const FooterHeading = styled.h5`
+  color: #0B539B;
+  font-family: 'Inter, sans-serif'; 
+  font-size: 16px; 
+  font-style: normal;
+  font-weight: 700;
+  line-height: 2px;
+  padding: 0 40px 0px 40px;
+  
+`;
+
+const StyledParagraph = styled.p`
+  color: #2B3136;
+  font-family: 'Inter, sans-serif';
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  padding: 0 40px 0 40px;
+
+
+`;
 
 function Footer() {
   return (
-    <StyledFooter>
-      <div>
+    <FooterContent>
+      <FooterColumn>
         <FooterHeading>Service hotline</FooterHeading>
-        <p>Support and counsell</p>
-        <p>0180 - 000000</p>
-        <p>Mon-Fri, 9 am - 5 pm</p>
-        <p>Or via our contact f</p>
-      </div>
+        <StyledParagraph>Support and counsell</StyledParagraph>
+        <StyledParagraph>0180 - 000000</StyledParagraph>
+        <StyledParagraph>Mon-Fri, 9 am - 5 pm</StyledParagraph>
+        <StyledParagraph>Or via our contact form</StyledParagraph>
+      </FooterColumn>
 
-      <div>
+      <FooterColumn>
         <FooterHeading>Shop Service</FooterHeading>
-        <p>Newsletter</p>
-        <p>Merchant login</p>
-        <p>Contact</p>
-        <p>Return</p>
-        <p>Repair request</p>
-      </div>
+        <StyledParagraph>Newsletter</StyledParagraph>
+        <StyledParagraph>Merchant login</StyledParagraph>
+        <StyledParagraph>Contact</StyledParagraph>
+        <StyledParagraph>Return</StyledParagraph>
+        <StyledParagraph>Repair request</StyledParagraph>
+      </FooterColumn>
 
-      <div>
+      <FooterColumn>
         <FooterHeading>Information</FooterHeading>
-        <p>General terms</p>
-        <p>Privacy</p>
-        <p>Shipping and payment</p>
-        <p>Right of rescission</p>
-        <p>About</p>
-      </div>
-    </StyledFooter>
+        <StyledParagraph>General terms</StyledParagraph>
+        <StyledParagraph>Privacy</StyledParagraph>
+        <StyledParagraph>Shipping and payment</StyledParagraph>
+        <StyledParagraph>Right of rescission</StyledParagraph>
+        <StyledParagraph>About</StyledParagraph>
+      </FooterColumn>
+    </FooterContent>
   );
 }
 
