95df7ef2a0f1f284aca679c0f4abe6d8283307f2
Annelieno1 <gydano1@hotmail.com>
2024-01-25 11:23:50 +0100

    progressed cartpage

---
+++ Frontend2Assignment/src/components/sections/cartPage/sections/CartContainer.jsx
@@ -0,0 +1,10 @@
+import CartProductsContainer from "../individuals/CartProductsContainer";
+
+const CartContainer = () => {
+    return (
+        <>
+            <CartProductsContainer />
+        </>);
+}
+
+export default CartContainer;
