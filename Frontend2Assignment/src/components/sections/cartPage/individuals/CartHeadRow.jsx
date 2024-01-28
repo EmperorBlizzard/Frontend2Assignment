97054bb4b844843e334c20f2a6c2dafa1227cc7f
Thomas Hallström <minimoto@hotmail.com>
2024-01-28 13:54:48 +0100

    Minor bug fix merging conflict

--- Frontend2Assignment/src/components/sections/cartPage/individuals/CartHeadRow.jsx
+++ Frontend2Assignment/src/components/sections/cartPage/individuals/CartHeadRow.jsx
@@ -1,21 +1,13 @@
 import styled from "styled-components";
 
 const Styleheadrow = styled.div`
-<<<<<<< HEAD
- 
-display: flex;
-padding-bottom: 0.625rem;
-align-items: flex-start;
-align-self: stretch;
-border-bottom: 1px solid var(--Border-Default, #798490);
 
-=======
 	display: flex;
 	padding-bottom: 0.625rem;
 	align-items: flex-start;
 	align-self: stretch;
 	border-bottom: 1px solid var(--Border-Default, #798490);
->>>>>>> 44f0a4a517992566e691b041db5c24cb05785db1
+
 `;
 const StyledProduct = styled.div`
 	display: flex;
@@ -25,24 +17,15 @@ const StyledProduct = styled.div`
 	color: var(--Typestyle-Headlines, #2b3136);
 	font-feature-settings: "clig" off, "liga" off;
 
-<<<<<<< HEAD
-color: var(--Typestyle-Headlines, #2B3136);
-font-feature-settings: 'clig' off, 'liga' off;
+	color: var(--Typestyle-Headlines, #2b3136);
+	font-feature-settings: "clig" off, "liga" off;
 
-/* Headlines/H5 */
-font-family: Inter;
-font-size: 1rem;
-font-style: normal;
-font-weight: 700;
-line-height: 1.3125rem; /* 131.25% */
-=======
 	/* Headlines/H5 */
 	font-family: Inter;
 	font-size: 1rem;
 	font-style: normal;
 	font-weight: 700;
 	line-height: 1.3125rem; /* 131.25% */
->>>>>>> 44f0a4a517992566e691b041db5c24cb05785db1
 `;
 const StyledQuantity = styled.div`
 	display: flex;
@@ -56,28 +39,19 @@ const StyledQuantity = styled.div`
 	width: 9.375rem;
 	align-items: flex-start;
 
-<<<<<<< HEAD
-color: var(--Typestyle-Headlines, #2B3136);
-font-feature-settings: 'clig' off, 'liga' off;
+	color: var(--Typestyle-Headlines, #2b3136);
+	font-feature-settings: "clig" off, "liga" off;
 
-display: flex;
-width: 9.375rem;
-align-items: flex-start;
+	display: flex;
+	width: 9.375rem;
+	align-items: flex-start;
 
-/* Headlines/H5 */
-font-family: Inter;
-font-size: 1rem;
-font-style: normal;
-font-weight: 700;
-line-height: 1.3125rem; /* 131.25% */
-=======
 	/* Headlines/H5 */
 	font-family: Inter;
 	font-size: 1rem;
 	font-style: normal;
 	font-weight: 700;
 	line-height: 1.3125rem; /* 131.25% */
->>>>>>> 44f0a4a517992566e691b041db5c24cb05785db1
 `;
 
 const StyledSubtotal = styled.div`
@@ -89,31 +63,6 @@ const StyledSubtotal = styled.div`
 	text-align: right;
 	font-feature-settings: "clig" off, "liga" off;
 
-<<<<<<< HEAD
-/* Headlines/H5 */
-font-family: Inter;
-font-size: 1rem;
-font-style: normal;
-font-weight: 700;
-line-height: 1.3125rem; /* 131.25% */
-`;
-
-const CartHeadRow = () => {
-
-    
-    return ( <Styleheadrow>
-        
-            <StyledProduct>Product</StyledProduct>
-            <StyledQuantity>Quantity</StyledQuantity>
-            <StyledSubtotal>Subtotal</StyledSubtotal>
-
-            
-            
-        
-    </Styleheadrow> );
-}
- 
-=======
 	/* Headlines/H5 */
 	font-family: Inter;
 	font-size: 1rem;
@@ -131,5 +80,4 @@ const CartHeadRow = () => {
 		</Styleheadrow>
 	);
 };
->>>>>>> 44f0a4a517992566e691b041db5c24cb05785db1
 export default CartHeadRow;
