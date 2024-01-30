f145ab867b871edd5d9ccee281332d8d52220e77
Tina-Lutti <tina.lutti@yh.nackademin.se>
2024-01-30 12:01:33 +0100

    Funktion för att se om något finns i Cart

--- Frontend2Assignment/src/components/sections/checkoutPage/sections/CheckoutCart.jsx
+++ Frontend2Assignment/src/components/sections/checkoutPage/sections/CheckoutCart.jsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import PurchaseButton from "../individual/PurchaseButton";
 import CheckoutCartItem from "./CheckoutCartItem";
 
+
+
 const CheckoutCartContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -18,17 +20,24 @@ const CheckoutCartContainer = styled.div`
   `
 
 const CheckoutCart = ({CheckoutCartItems}) =>{
+    console.log("Rendering CheckoutCart");
+
+    const hasItemsInCart = CheckoutCartItems && CheckoutCartItems.length > 0;
     return(
         
         <CheckoutCartContainer>
             <h2>Checkout cart</h2>
-           {CheckoutCartItems && CheckoutCartItems.length >0 && CheckoutCartItems.map((item) =>(
-            <CheckoutCartItem key={item.product.id} product={item.product} quantity ={item.quantity} />
-        ))}
-        <PurchaseButton />
-        </CheckoutCartContainer>
+           {hasItemsInCart ?(
+            CheckoutCartItem.map((item) => (
+            <CartContainer key={item.product.id} product ={item.product} quantity ={item.quantity} />
+            ))
 
-        
+           ) : (
+            <p>Your cart is empty</p>
+        )}
+
+        <PurchaseButton />
+        </CheckoutCartContainer>      
 
     );
 
--- Frontend2Assignment/src/components/sections/checkoutPage/sections/CheckoutCartItem.jsx
+++ Frontend2Assignment/src/components/sections/checkoutPage/sections/CheckoutCartItem.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const CheckoutCartItem = ({product, price, quantity}) =>{
+const CheckoutCartItem = ({product, price, quantity, isPacked}) =>{
     return(
         <div>
             <p>{product.title}</p>
             <p>{product.price}</p>
             <p>{product.quantity}</p>
+            <p>{isPacked ?'yes' : 'No'}</p>
 
         </div>
 
