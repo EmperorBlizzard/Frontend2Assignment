8c54c5227778360575332a4fc56e696e714912d5
EmperorBlizzard <emil111@live.se>
2024-02-01 17:50:40 +0100

    Started with startPage layout

---
+++ Frontend2Assignment/src/components/sections/startPage/individuals/NewProducts.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "../../../../styling/StartPageStyle.module.css"
+
+function NewProducts() {
+  return (
+    <div>NewProducts</div>
+  )
+}
+
+export default NewProducts
---
+++ Frontend2Assignment/src/components/sections/startPage/individuals/Slideshow.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "../../../../styling/StartPageStyle.module.css"
+
+function Slideshow() {
+  return (
+    <div>Slideshow</div>
+  )
+}
+
+export default Slideshow
