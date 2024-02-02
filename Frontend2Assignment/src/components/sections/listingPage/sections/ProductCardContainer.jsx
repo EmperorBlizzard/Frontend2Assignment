1152531f223202406a04103aeda7b62a88b9cc3a
MrMinken <minimoto@hotmail.com>
2024-02-02 09:09:03 +0100

    Minor fix in productCard

--- Frontend2Assignment/src/components/sections/listingPage/sections/ProductCardContainer.jsx
+++ Frontend2Assignment/src/components/sections/listingPage/sections/ProductCardContainer.jsx
@@ -5,77 +5,74 @@ import GetApi from "../../../../Products/api";
 import PropTypes from "prop-types";
 
 const StyledProductCardContainer = styled.div`
-	display: flex;
-	flex-direction: row;
-	align-items: flex-Start;
-	flex-wrap: wrap;
-	gap: 32px;
-	@media (max-width: 600px) {
-		flex-direction: column;
-	}
+  display: flex;
+  flex-direction: row;
+  align-items: flex-Start;
+  flex-wrap: wrap;
+  gap: 32px;
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
 `;
 
 export const ProductContext = createContext(null);
 
 const ProductCardContainer = ({ filter }) => {
-	const [products, setProducts] = useState(null);
-	const [itemInCart, setItemInCart] = useState([]);
+  const [products, setProducts] = useState(null);
+  const [itemInCart, setItemInCart] = useState([]);
 
-	const handleButtonClick = (buttonInfo) => {
-		const itemIndex = itemInCart.findIndex((item) => item.id === buttonInfo.id);
-		if (itemIndex !== -1) {
-			const upDatedList = [...itemInCart];
-			upDatedList[itemIndex].amountOfProducts += 1;
-			setItemInCart(upDatedList);
-		} else {
-			const item = {
-				id: buttonInfo.id,
-				productName: buttonInfo.attributes.productName,
-				price: buttonInfo.attributes.price,
-				amountOfProducts: 1,
-			};
-			const upDatedList = [...itemInCart, item];
-			setItemInCart(upDatedList);
-			console.log(itemInCart);
-		}
-	};
+  const handleButtonClick = (buttonInfo) => {
+    const itemIndex = itemInCart.findIndex((item) => item.id === buttonInfo.id);
+    if (itemIndex !== -1) {
+      const upDatedList = [...itemInCart];
+      upDatedList[itemIndex].amountOfProducts += 1;
+      setItemInCart(upDatedList);
+    } else {
+      const item = {
+        id: buttonInfo.id,
+        productName: buttonInfo.attributes.productName,
+        price: buttonInfo.attributes.price,
+        amountOfProducts: 1,
+      };
+      const upDatedList = [...itemInCart, item];
+      setItemInCart(upDatedList);
+      console.log(itemInCart);
+    }
+  };
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const result = await GetApi(filter);
-				setProducts(result.data.data);
-			} catch (error) {
-				console.error("Error fetching data: ", error);
-			}
-		};
-		if (filter !== "") {
-			fetchData();
-		}
-	}, [filter]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await GetApi(filter);
+        setProducts(result.data.data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+    if (filter !== "") {
+      fetchData();
+    }
+  }, [filter]);
 
-	const Kolla = () => {
-		if (products) {
-			return products.map((product) => (
-				<ProductContext.Provider key={product.id} value={product}>
-					<ProductCard onButtonClick={handleButtonClick} />
-				</ProductContext.Provider>
-			));
-		}
-	};
+  const Kolla = () => {
+    if (products) {
+      return products.map((product) => (
+        <ProductContext.Provider key={product.id} value={product}>
+          <ProductCard onButtonClick={handleButtonClick} />
+        </ProductContext.Provider>
+      ));
+    }
+  };
 
-	return (
-		<StyledProductCardContainer>
-		<div>
-		<Kolla />
-		</div>
-			
-		</StyledProductCardContainer>
-	);
+  return (
+    <StyledProductCardContainer>
+      <Kolla />
+    </StyledProductCardContainer>
+  );
 };
 
 ProductCardContainer.propTypes = {
-	filter: PropTypes.string,
+  filter: PropTypes.string,
 };
 
 export default ProductCardContainer;
