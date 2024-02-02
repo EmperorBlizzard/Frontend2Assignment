d02bdca161db56ac9988c650d2f9fe9d9cd6f1c2
Tina-Lutti <tina.lutti@yh.nackademin.se>
2024-02-02 15:24:21 +0100

    CheckoutCart function

--- Frontend2Assignment/src/Pages/CheckoutPage.jsx
+++ Frontend2Assignment/src/Pages/CheckoutPage.jsx
@@ -1,75 +1,75 @@
 import Header from "../components/sections/Header";
 import Footer from "../components/sections/Footer";
-import BillingAddress from "../components/sections/checkoutPage/sections/BillingAddress.jsx"; 
+import BillingAddress from "../components/sections/checkoutPage/sections/BillingAddress.jsx";
 import ShippingAddress from "../components/sections/checkoutPage/sections/ShippingAddress.jsx";
 import CheckoutCart from "../components/sections/checkoutPage/sections/CheckoutCart.jsx";
 import PaymentMethod from "../components/sections/checkoutPage/sections/PaymentMethod.jsx";
 import styled from "styled-components";
+import { useContext } from "react";
+import { CartContext } from "../App.jsx";
+import ProductCardCheckout from "../components/sections/checkoutPage/sections/ProductCardCheckout.jsx";
 
 const StyledCheckout = styled.div`
-  
   max-width: 55.9375rem;
   width: 100%;
   margin: 5rem;
-  
 
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-	@media (max-width: 37.5rem) {
-		flex-direction: column;
-	}
+  @media (max-width: 37.5rem) {
+    flex-direction: column;
+  }
 `;
 
 const SectionContainerText = styled.div`
   justify-content: space-between;
   align-items: flex-start;
   align-self: stretch;
-  
+
   margin-bottom: 2rem;
   font-weight: 600;
-  line-height: 1.75rem; 
-  letter-spacing: -0.03rem; 
-  
-  color: #2B3136; 
-  font-family: 'Inter', sans-serif; 
+  line-height: 1.75rem;
+  letter-spacing: -0.03rem;
+
+  color: #2b3136;
+  font-family: "Inter", sans-serif;
   font-size: 2rem;
   box-sizing: border-box;
   width: 64rem;
   padding: 1rem;
   margin: 1rem;
-   
-`; 
-
-
-
-
+`;
 
 const CheckOutPage = () => {
+   const Name = useContext(CartContext);
+  const NameList = Name.itemsInCart; 
+
+     const Mapping = () => {
+      return NameList.map((namn) => (
+        <div key={namn.id}>
+          <ProductCardCheckout card={namn} />
+        </div> 
+   )); 
+      };
+
   return (
     <>
       <Header />
 
       <StyledCheckout>
-       <SectionContainerText>
+        <SectionContainerText>
           <p>Complete order</p>
-          
-        </SectionContainerText> 
+        </SectionContainerText>
 
-       
-          <BillingAddress />
-          
+        <BillingAddress />
 
-       
-          <ShippingAddress />
-       
+        <ShippingAddress />
+        <Mapping />
 
-       
-          <CheckoutCart />
-       
+        <CheckoutCart />
 
         <PaymentMethod />
-
       </StyledCheckout>
 
       <Footer />
