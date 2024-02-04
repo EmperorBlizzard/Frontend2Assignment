827f6aa269fd67aaed86a630543e7a2690d05def
Annelieno1 <gydano1@hotmail.com>
2024-02-04 23:09:20 +0100

    continued working on shopping cart

--- Frontend2Assignment/src/components/sections/cartPage/sections/CartContent.jsx
+++ Frontend2Assignment/src/components/sections/cartPage/sections/CartContent.jsx
@@ -4,7 +4,6 @@ import Cartheadrow from "../individuals/CartHeadRow";
 import CartProductCardContainer from "./CartProductCardContainer";
 import InputPlusLink from "../individuals/Inputpluslink";
 import products from "../../../../Products/Products.json"
-import CartProductDescription from "../individuals/CartProductDescription";
 import SummaryContainer from "./SummaryContainer";
 
 export const TempCartContext = createContext(null)
@@ -16,16 +15,7 @@ function CartContent() {
     return (
         <>
             <Cartheadrow />
-            {
-                products.products.map(product => {
-                    return (
-                        <TempCartContext.Provider id={product.id} value={product}>
-                            <CartProductCardContainer />
-                        </TempCartContext.Provider>
-                    )
-                })
-            }
-           
+            <CartProductCardContainer />
             <InputPlusLink />
             
         </>
--- Frontend2Assignment/src/components/sections/cartPage/sections/CartProductCardContainer.jsx
+++ Frontend2Assignment/src/components/sections/cartPage/sections/CartProductCardContainer.jsx
@@ -1,15 +1,33 @@
+import { useContext } from "react";
 import CartProductDescription from "../individuals/CartProductDescription";
 import QuantitySelector from "../individuals/QuantitySelector";
 import styled from "styled-components";
 
+
+
 const CartProductCardContainerDiv = styled.div`
     display: flex;
-`
+`;
 
 function CartProductCardContainer() {
+
+    const card = useContext(Cartcontext);
+    const NameList = card.itemsInCart;
+
+    const Test = () => {
+        return (
+            <>
+            {NameList.map((card) =>(
+                <CartProductDescription key={card.id} card={card} />
+            ))}
+            
+            </>
+        );
+    };
     return (
         <CartProductCardContainerDiv>
-            <CartProductDescription />
+         <Test/>
+            
             <QuantitySelector />
         </CartProductCardContainerDiv>
     )
