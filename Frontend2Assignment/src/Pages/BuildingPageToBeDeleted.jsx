b971544b3bd6a358b970476e8096776bd5aa4d8d
Tina-Lutti <tina.lutti@yh.nackademin.se>
2024-02-08 14:25:36 +0100

    stockfunction implemented at PurchaseButton

--- Frontend2Assignment/src/Pages/BuildingPageToBeDeleted.jsx
+++ Frontend2Assignment/src/Pages/BuildingPageToBeDeleted.jsx
@@ -1,38 +1,15 @@
+import React from "react";
 import Header from "../components/sections/Header";
 import Footer from "../components/sections/Footer";
-import PutApi from "../Products/PutApi";
-import { useContext } from "react";
-import { CartContext } from "../App";
 
 function Building() {
-	const {itemsInCart, setItemsInCart} = useContext(CartContext);
-  console.log(itemsInCart)
-	const handleClick = async () => {
-		try {
-      for (const element of itemsInCart) {
-        let newStock = element.stock - element.amountOfProducts
-        const id = element.id
-        if (newStock <=0) {
-          newStock = 0
-        }
-        console.log(newStock)
-        console.log(id)
-        await PutApi(id, newStock);
-
-      }
-      setItemsInCart([])
-		} catch (error) {
-			console.error(error);
-		}
-	};
-	return (
-		<>
-			<Header />
-			<h1>building page</h1>
-			<button onClick={handleClick}>Click me</button>
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <h1>building page</h1>
+      <Footer />
+    </>
+  );
 }
 
 export default Building;
